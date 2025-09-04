f664886e683a569a3dcfedaeef2891769bdc1f23
PytoByte <vovamarkidonov2006vova@gmail.com>
2025-09-04 10:18:32 +0700

    complete sorting and tests

--- Task_1_1_1/src/main/java/ru/nsu/vmarkidonov/Sort.java
+++ Task_1_1_1/src/main/java/ru/nsu/vmarkidonov/Sort.java
@@ -1,81 +1,89 @@
 package ru.nsu.vmarkidonov;
 
-// TODO: documentation and tests, make heap as class
-
 /**
  * Class, containing sorting functions
  */
-class Sort {
-    public static int parent(int index) {
-        if (index % 2 == 0) {
-            return index / 2 - 1;
-        } else {
-            return index / 2;
-        }
-    }
-
+public class Sort {
+    /**
+     * Returns child indices for given index in heap
+     *
+     * @param index current index
+     * @return array with left and right child indices
+     */
     public static int[] childs(int index) {
-        return new int[] {index*2+1, (index+1)*2};
+        return new int[]{index * 2 + 1, index * 2 + 2};
     }
 
+    /**
+     * Swaps two elements in array
+     *
+     * @param array input array
+     * @param i     first index
+     * @param j     second index
+     */
     public static void swap(int[] array, int i, int j) {
         int tmp = array[i];
         array[i] = array[j];
         array[j] = tmp;
     }
 
-    public static void heapify_down(int[] heap, int i, int end) {
+    /**
+     * Restores heap property by moving element down the heap
+     *
+     * @param heap heap array
+     * @param i    current index
+     * @param end  end index (exclusive)
+     */
+    public static void heapifyDown(int[] heap, int i, int end) {
         while (true) {
-            printarr(heap);
-            int[] cs = childs(i);
+            int[] children = childs(i);
+            int leftChild = children[0];
+            int rightChild = children[1];
 
-            if (cs[0] >= end && cs[1] >= end) {
+            if (leftChild >= end) {
                 break;
             }
 
-            int swapping_child;
-            if (heap[cs[0]] <=> heap[i] || cs[1] >= end) {
-                swapping_child = cs[0];
-            } else {
-                swapping_child = cs[1];
+            int largestChild = leftChild;
+            if (rightChild < end && heap[rightChild] > heap[leftChild]) {
+                largestChild = rightChild;
             }
 
-            swap(heap, i, swapping_child);
-            i = swapping_child;
+            if (heap[largestChild] > heap[i]) {
+                swap(heap, i, largestChild);
+                i = largestChild;
+            } else {
+                break;
+            }
         }
     }
 
-    public static int[] makeheap(int[] array) {
-        int[] heap = array.clone();
-        for (int i = 0; i < array.length; i++) {
-            heapify_down(heap, i, array.length);
+    /**
+     * Builds max-heap from array
+     *
+     * @param array input array
+     */
+    public static void makeHeap(int[] array) {
+        for (int i = array.length / 2 - 1; i >= 0; i--) {
+            heapifyDown(array, i, array.length);
         }
-        return heap;
-    }
-
-    public static int heappop(int[] heap, int end) {
-        swap(heap, 0 , end-1);
-        heapify_down(heap, 0, end-1);
-        return heap[end-1];
     }
 
     /**
      * Heap sort aka pyramid sort
      *
-     * @param array shuffled array
+     * @param array array to be sorted
      */
-    public static void heapsort(int[] array) {
-        int[] heap = makeheap(array);
-        printarr(heap);
-        for (int i = 0; i < heap.length; i++) {
-            array[i] = heappop(heap, heap.length-i);
+    public static void heapSort(int[] array) {
+        if (array == null || array.length <= 1) {
+            return;
         }
-    }
 
-    public static void printarr(int[] array) {
-        for (int j : array) {
-            System.out.printf("%d ", j);
+        makeHeap(array);
+
+        for (int i = array.length - 1; i > 0; i--) {
+            swap(array, 0, i);
+            heapifyDown(array, 0, i);
         }
-        System.out.print("\n");
     }
 }
--- Task_1_1_1/src/test/java/ru/nsu/vmarkidonov/SortTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/vmarkidonov/SortTest.java
@@ -1,22 +1,70 @@
 package ru.nsu.vmarkidonov;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-// TODO: tests
+import java.util.Arrays;
+import java.util.Random;
 
 class SortTest {
     @Test
-    void heapsort() {
+    void heapsortPositive() {
         int[] array = {1, 6, 5, 3, 7};
-        Sort.heapsort(array);
-
-        for (int j : array) {
-            System.out.printf("%d ", j);
-        }
-        System.out.print("\n");
-
+        Sort.heapSort(array);
         assertArrayEquals(new int[]{1, 3, 5, 6, 7}, array);
+    }
+
+    @Test
+    void heapsortNegative() {
+        int[] array = {-1, -6, -5, -3, -7};
+        Sort.heapSort(array);
+        assertArrayEquals(new int[]{-7, -6, -5, -3, -1}, array);
+    }
+
+    @Test
+    void heapsortPositiveNegativeZero() {
+        int[] array = {1, 4, 10, -2, -6, 0, -5};
+        Sort.heapSort(array);
+        assertArrayEquals(new int[]{-6, -5, -2, 0, 1, 4, 10}, array);
+    }
+
+    @Test
+    void heapsortEmpty() {
+        int[] array = {};
+        Sort.heapSort(array);
+        assertArrayEquals(new int[]{}, array);
+    }
+
+    @Test
+    void heapsortNull() {
+        Sort.heapSort(null);
+    }
+
+    @Test
+    void heapsortOneElem() {
+        int[] array = {10};
+        Sort.heapSort(array);
+        assertArrayEquals(new int[]{10}, array);
+    }
+
+    @Test
+    void heapsortBigArray() {
+        int size = 1000000;
+        Random random = new Random();
+        int[] array = new int[size];
+        int[] expected = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            int value = random.nextInt(size) - 500000;
+            array[i] = value;
+            expected[i] = value;
+        }
+
+        Sort.heapSort(array);
+
+        Arrays.sort(expected);
+        
+        assertArrayEquals(expected, array);
     }
 }
