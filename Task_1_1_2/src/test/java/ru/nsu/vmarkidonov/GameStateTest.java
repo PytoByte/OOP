17943a0eac92c6c03d844604fc54a9fa70f8c7c9
PytoByte <vovamarkidonov2006vova@gmail.com>
2025-09-20 16:56:53 +0700

    Reviewdog fixes

--- Task_1_1_2/src/test/java/ru/nsu/vmarkidonov/GameStateTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/vmarkidonov/GameStateTest.java
@@ -1,7 +1,7 @@
 package ru.nsu.vmarkidonov;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
