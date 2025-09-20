244f9e0036acb90396e3db383947a37a6f89bc06
PytoByte <vovamarkidonov2006vova@gmail.com>
2025-09-20 21:25:29 +0700

    Added "Add" tests

--- Task_1_1_3/src/test/java/ru/nsu/vmarkidonov/tokens/AddTest.java
+++ Task_1_1_3/src/test/java/ru/nsu/vmarkidonov/tokens/AddTest.java
@@ -2,6 +2,7 @@ package ru.nsu.vmarkidonov.tokens;
 
 import org.junit.jupiter.api.Test;
 import ru.nsu.vmarkidonov.Expression;
+import ru.nsu.vmarkidonov.tokens.Number;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,13 +16,19 @@ class AddTest {
 
     @Test
     void derivative() {
+        Expression e = new Add(new Number(1), new Variable("x"));
+        assertEquals("(0+0)", e.derivative("x").toString());
     }
 
     @Test
     void testClone() {
+        Expression e = new Add(new Number(1), new Number(1));
+        assertNotSame(e, e.clone());
     }
 
     @Test
     void testToString() {
+        Expression e = new Add(new Number(1), new Number(1));
+        assertEquals("(1+1)", e.toString());
     }
 }
