5a7e15bd99866c2a01a0af6dbdb6d05906c79d2c
PytoByte <vovamarkidonov2006vova@gmail.com>
2025-09-20 16:46:44 +0700

    Review dog fixes

--- Task_1_1_2/src/test/java/ru/nsu/vmarkidonov/DeckTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/vmarkidonov/DeckTest.java
@@ -1,8 +1,8 @@
 package ru.nsu.vmarkidonov;
 
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
--- Task_1_1_2/src/test/java/ru/nsu/vmarkidonov/HandTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/vmarkidonov/HandTest.java
@@ -1,13 +1,12 @@
 package ru.nsu.vmarkidonov;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
+import org.junit.jupiter.api.Test;
 
 class HandTest {
 
