47ef2d6cf5c55398f9291f0b46d33199e7103593
PytoByte <vovamarkidonov2006vova@gmail.com>
2025-11-22 13:14:02 +0700

    return 34 gb file

--- Task_1_3_1/src/test/java/SubstringFinderTest.java
+++ Task_1_3_1/src/test/java/SubstringFinderTest.java
@@ -62,7 +62,7 @@ public class SubstringFinderTest {
     void testLargeFile(@TempDir Path tempDir) throws IOException {
         Path testFile = tempDir.resolve("large_test.txt");
 
-        String largeContent = ("x".repeat(1024 * 1024));
+        String largeContent = ("x".repeat(1024 * 1024 * 1024));
         for (int i = 0; i < 34; i++) {
             Files.write(testFile, largeContent.getBytes(),
                     StandardOpenOption.CREATE,
