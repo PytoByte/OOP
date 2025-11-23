1f6ef03f643dd9bf8950bf2c8fd992e95fac11b5
PytoByte <vovamarkidonov2006vova@gmail.com>
2025-11-23 17:32:23 +0700

    WIP: Subject

--- Task_1_4_1/src/main/java/Gradebook.java
+++ Task_1_4_1/src/main/java/Gradebook.java
@@ -1,3 +1,11 @@
+import java.util.LinkedList;
+
 public class Gradebook {
-    
+    private final LinkedList<Semester> semesters = new LinkedList<>();
+
+    public void addSemester(Semester semester) {
+        semesters.add(semester);
+    }
+
+
 }
---
+++ Task_1_4_1/src/main/java/Subject.java
@@ -0,0 +1,18 @@
+public class Subject {
+    private String name;
+    private int assessment;
+    private int marks;
+    private int testsCount;
+
+    /*
+    test
+    colloquium
+    EXAM
+    DIFF_CREDIT
+    CREDIT
+    PRAC_REP_PROT
+    FIN_QUAL_WORK_PROT
+     */
+
+
+}
