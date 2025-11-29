4d080e9e5237cdaa5ddb21f2958fe33a594d3565
PytoByte <vovamarkidonov2006vova@gmail.com>
2025-11-29 13:18:53 +0700

    import fix

--- Task_1_4_1/src/test/java/GradeTest.java
+++ Task_1_4_1/src/test/java/GradeTest.java
@@ -1,6 +1,6 @@
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 /**
  * Test for grade class.
