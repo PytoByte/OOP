70e1dbf7390ce8d0f638f33417e6f2a261915afe
PytoByte <vovamarkidonov2006vova@gmail.com>
2026-03-08 00:20:17 +0700

    review dog 3

--- Task_2_2_1/src/test/java/ChannelTest.java
+++ Task_2_2_1/src/test/java/ChannelTest.java
@@ -1,9 +1,9 @@
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
 
 class ChannelTest {
 
