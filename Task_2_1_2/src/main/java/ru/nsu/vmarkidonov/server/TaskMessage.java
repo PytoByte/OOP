44827437ff0ba12cdcd050c1a71792fd8fc2e9c2
PytoByte <vovamarkidonov2006vova@gmail.com>
2026-05-30 23:22:44 +0700

    javadoc

--- Task_2_1_2/src/main/java/ru/nsu/vmarkidonov/server/TaskMessage.java
+++ Task_2_1_2/src/main/java/ru/nsu/vmarkidonov/server/TaskMessage.java
@@ -5,13 +5,27 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import ru.nsu.vmarkidonov.Protocol;
 
+/**
+ * Объект сообщение, описывающий выдачу задачи.
+ */
 public class TaskMessage {
     private final long[] numbers;
 
+    /**
+     * Базовый конструктор.
+     *
+     * @param numbers массив чисел, переданных клиенту
+     */
     public TaskMessage(long[] numbers) {
         this.numbers = numbers;
     }
 
+    /**
+     * Сериализация в массив байт.
+     *
+     * @return массив байт
+     * @throws IOException при ошибке сериализация
+     */
     public byte[] toByteArray() throws IOException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         try (DataOutputStream dos = new DataOutputStream(baos)) {
